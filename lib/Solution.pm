950081bd712c490e8c900c5bbfff07536a33de15
Sanko Robinson <sanko@cpan.org>
2009-12-24 02:40:22 +0800

    Document custom tag creation and c+p Liquid's quick start guide

--- lib/Solution.pm
+++ lib/Solution.pm
@@ -84,7 +84,30 @@ objects.
 
 =back
 
-=head2 Why should I use Liquid?
+=head2 Getting Started
+
+It’s very simple to get started with L<Solution|Solution>. Just as in Liquid,
+templates are built and used in two steps: Parse and Render.
+
+For an overview of the Liquid/Solution syntax, please read
+Liquid for Designers (it's linked to in the L<See Also|Solution/"See Also">
+section below).
+
+    # Parses and compiles the template
+    my $template = Solution::Template->parse('Hi, {{name}}!');
+
+    # Renders the output => "Hi, Sanko!"
+    $template->render({ name => 'Sanko' });
+
+The C<parse> step creates a fully compiled template which can be re-used as
+often as you like. You can store it in memory or in a cache for faster
+rendering later.
+
+All parameters you want L<Solution> to work with have to be passed as
+parameters to the render method. L<Solution> is a closed ecosystem; it does
+not know about your local, instance, and global variables.
+
+=head2 Why should I use Solution?
 
 =over 4
 
@@ -105,7 +128,7 @@ past seasons of I<Dr. Who>.
 
 =back
 
-=head2 Why shouldn't I use Liquid?
+=head2 Why shouldn't I use Solution?
 
 =over 4
 
@@ -119,7 +142,7 @@ L<See Also|Solution/"Other Template Engines"> section.
 
 =back
 
-=head2 Why a new Top Level Namespace?
+=head2 Ugh! Why a new Top Level Namespace?
 
 I really don't have a good reason but I promise to send myself to bed without
 dinner as punishment.
@@ -127,6 +150,119 @@ dinner as punishment.
 Eh... The name L<Solution|Solution> is a reference to the classical states of
 matter not an absolute answer. The engine is based on the Liquid Template
 Engine in Ruby but with a few extra things tossed it.
+
+=head1 Extending Solution
+
+=head2 Custom Filters
+
+TODO
+
+=head2 Create Your Own Tags
+
+To create a new tag, simply inherit from L<Solution::Tag|Solution::Tag> and
+register your block L<globally|Liquid/"register_tag"> or locally with
+L<Liquid::Template|Liquid::Template/"register_tag">.
+
+    package SolutionX::Tag::Random;
+    use strict;
+    use warnings;
+    use Carp qw[confess];
+    our @ISA = qw[Solution::Tag];
+    Solution->register_tag('random') if $Solution::VERSION;
+
+    sub new {
+        my ($class, $args, $tokens) = @_;
+        $args->{'attrs'} ||= 50;
+        my $self = bless {name     => 'rand-' . $args->{'attrs'},
+                          tag_name => $args->{'tag_name'},
+                          max      => $args->{'attrs'},
+                          parent   => $args->{'parent'},
+                          markup   => $args->{'markup'},
+        }, $class;
+        return $self;
+    }
+
+    sub render {
+        my ($self) = @_;
+        return int rand $self->resolve($self->{'max'});
+    }
+    1;
+
+Using this new tag is as simple as...
+
+    use Solution;
+    use SolutionX::Tag::Random;
+
+    print Solution::Template->parse('{% random max %}')->render({max => 30});
+
+This will print a random integer between C<0> and C<30>.
+
+=head2 Creating Your Own Tag Blocks
+
+Block-like tags are very similar to L<simple|Solution/"Create Your Own Tags">.
+Inherit from L<Solution::Tag|Solution::Tag> and register your block
+L<globally|Liquid/"register_tag"> or locally with
+L<Liquid::Template|Liquid::Template/"register_tag">.
+
+    package SolutionX::Tag::Large::Hadron::Collider;
+    use strict;
+    use warnings;
+    use Carp qw[confess];
+    our @ISA = qw[Solution::Tag];
+    Solution->register_tag('lhc') if $Solution::VERSION;
+
+    sub new {
+        my ($class, $args, $tokens) = @_;
+        my $self = bless {name     => 'rand-' . $args->{'attrs'},
+                          tag_name => $args->{'tag_name'},
+                          odds     => $args->{'attrs'},
+                          parent   => $args->{'parent'},
+                          markup   => $args->{'markup'},
+                          end_tag  => 'end' . $args->{'tag_name'}
+        }, $class;
+        $self->parse($tokens);
+        return $self;
+    }
+
+    sub render {
+        my ($self) = @_;
+        return if int rand $self->resolve($self->{'odds'});
+        return join '', @{$self->{'nodelist'}};
+    }
+    1;
+
+Using this example tag...
+
+    use Solution;
+    use SolutionX::Tag::Large::Hadron::Collider;
+
+    warn Solution::Template->parse(q[{% lhc 2 %}Now, that's money well spent!{% endlhc %}])->render();
+
+Just like the real thing, our C<lhc> tag works only 50% of the time.
+
+The biggest changes between this and the
+L<random tag|Solution/"Create Your Own Tags"> we build above are in the
+constructor.
+
+The extra C<end_tag> attribute in the object's reference lets the parser know
+that this is a block that will slurp until the end tag is found. In our
+example, we use C<'end' . $args->{'tag_name'}> because you may eventually
+subclass this tag (as SolutionX::Tag::Vehicle::Ford, for example) and let it
+inherit this constructor. Now that we're sure the parser knows what to look
+for, we go ahead and continue L<parsing|Liquid::Template/"parse"> the list of
+tokens. The parser will shove child nodes (L<tags|Solution::Tag>,
+L<variables|Solution::Variable>, and simple strings) onto your stack until the
+C<end_tag> is found.
+
+In the render step, we must return the strigification of all child nodes
+pushed onto the stack by the parser.
+
+=head2 Creating Your Own Tag Blocks
+
+The internals are still kinda rough around this bit so documenting it is on my
+TODO list. If you're a glutton for punishment, I guess you can skim the source
+for the L<if tag|Solution::Tag::If> and its subclass, the
+L<unless tag|Solution::Tag::Unless>.
 
 =head1 See Also
 
