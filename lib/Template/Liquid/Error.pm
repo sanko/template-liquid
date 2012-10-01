2527eeb3fc3d0a2c1bf57e9f416bb74c426b5ef0
Sanko Robinson <sanko@cpan.org>
2012-10-01 01:03:06 -0400

    Adding stub docs for internals (TLContext and TLError)

--- lib/Template/Liquid/Error.pm
+++ lib/Template/Liquid/Error.pm
@@ -46,3 +46,38 @@ our @ISA = qw'Template::Liquid::Error';
 package Template::Liquid::StackLevelError;
 our @ISA = qw'Template::Liquid::Error';
 1;
+
+=cut
+
+=head1 NAME
+
+Template::Liquid::Error - General Purpose Error Object
+
+=head1 Description
+
+This is really only to be used internally.
+
+=head1 Author
+
+Sanko Robinson <sanko@cpan.org> - http://sankorobinson.com/
+
+CPAN ID: SANKO
+
+=head1 License and Legal
+
+Copyright (C) 2009-2012 by Sanko Robinson E<lt>sanko@cpan.orgE<gt>
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of
+L<The Artistic License 2.0|http://www.perlfoundation.org/artistic_license_2_0>.
+See the F<LICENSE> file included with this distribution or
+L<notes on the Artistic License 2.0|http://www.perlfoundation.org/artistic_2_0_notes>
+for clarification.
+
+When separated from the distribution, all original POD documentation is
+covered by the
+L<Creative Commons Attribution-Share Alike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/us/legalcode>.
+See the
+L<clarification of the CCA-SA3.0|http://creativecommons.org/licenses/by-sa/3.0/us/>.
+
+=cut
